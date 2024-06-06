5f35fea20f4f9b95ae6ee54ff0028e21d96d1738
ANJANA BANERJEE <141236447+Anjana940@users.noreply.github.com>
2024-06-06 17:38:48 +0530

    TodoWrapper.js

--- src/components/TodoWrapper.js
+++ src/components/TodoWrapper.js
@@ -15,7 +15,7 @@ export const TodoWrapper = () => {
         setTodos(todos.map(todo => todo.id===id? {...todo, completed: !todo.completed}: todo))
     }
     const deleteTodo=id=>{
-        setTodos(todos.filter(todo => todo.id !=id))
+        setTodos(todos.filter(todo => todo.id !==id))
     }
     const editTodo=id=>{
         setTodos(todos.map(todo => todo.id ===id ? {todo, isEditing:  !todo.isEditing} :todo ))
